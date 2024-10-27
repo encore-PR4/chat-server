208141c4e3c27fa8b55b26940306e407bc32da90
Jeonghoon2 <ukidd12@naver.com>
2024-10-28 00:31:51 +0900

    fix prod-sh

--- start-prod.sh
+++ start-prod.sh
@@ -5,19 +5,19 @@ DOCKER_COMPOSE_FILE='docker-compose.prod.yml'
 
 # 기존 서버 및 데이터베이스 컨테이너 종료 및 제거
 echo "기존 서버 및 데이터베이스 컨테이너를 종료하고 제거합니다."
-/usr/local/bin/docker-compose -f $DOCKER_COMPOSE_FILE down
+docker-compose -f $DOCKER_COMPOSE_FILE down
 
 ls
 
 # 도커 컴포즈로 이미지를 캐시 없이 빌드
 echo "캐시 없이 도커 이미지를 빌드합니다."
-/usr/local/bin/docker-compose -f $DOCKER_COMPOSE_FILE build --no-cache
+docker-compose -f $DOCKER_COMPOSE_FILE build --no-cache
 
 ls
 
 # 도커 컴포즈로 새로운 서버 및 데이터베이스 컨테이너 실행
 echo "새로운 도커 컴포즈 컨테이너를 시작합니다."
-/usr/local/bin/docker-compose -f $DOCKER_COMPOSE_FILE up -d
+docker-compose -f $DOCKER_COMPOSE_FILE up -d
 
 ls
 
